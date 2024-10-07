446eb7b1b85aba592196034e97aed9132bd34cb9
OUROUA M. Mounib <ourouamounib@gmail.com>
2024-10-07 22:42:03 +0200

    False warning

--- src/main/java/epsi/App.java
+++ src/main/java/epsi/App.java
@@ -1,20 +1,21 @@
-package epsi;
-
 import java.util.Arrays;
 
 /**
  * Hello world!
  *
- * *
  */
+@SuppressWarnings("A false warning for testing")
 public class App {
-    public static void main( String[] args )
-    {
-        int result = compute(args);
+    public static void main(String[] args) {
+        Nested instance = new Nested();
+        int result = (int) instance.first(args);
         System.out.println("The result is : " + result);
     }
 
-    public static int compute( String[] args) {
-        return new CalculatorService().add(Arrays.asList(Integer.valueOf(args[0]), Integer.valueOf(args[1])));
+    static class Nested<T> {
+        public T first(T[] args) {
+            return args[0];
+        }
     }
+
 }
